a42ef7e621406ea513a89a25c11218a0d0538a21
smalltur <15159896145@163.com>
2024-09-02 09:53:10 +0800

    1

--- MUD-VVS.cpp
+++ MUD-VVS.cpp
@@ -1,12 +1,6 @@
 ﻿// MUD-VVS.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
