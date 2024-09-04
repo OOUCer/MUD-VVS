94bc74f0e5d7ccdafe0cef033827e2b41c516e0a
smalltur <15159896145@163.com>
2024-09-04 17:43:33 +0800

    添加char中改变地点的实现

--- character.cpp
+++ character.cpp
@@ -261,7 +261,35 @@ int Linchong::getPlace() const
 
 void Linchong::changePlace()
 {
-    // Implement the logic to change the place of the character.
+    cout << "你要去哪：" << endl;
+    int placeflag;
+    cin >> placeflag;
+    cout << "你来到了";
+    place = placeflag;
+    switch (place)
+    {
+    case 1:
+        cout << "茶酒店";
+        break;
+    case 2:
+        cout << "武馆";
+        break;
+    case 3:
+        cout << "营房";
+        break;
+    case 4:
+        cout << "草料场";
+        break;
+    case 5:
+        cout << "茶酒店";
+        break;
+    case 6:
+        cout << "市井";
+        break;
+    case 7:
+        cout << "古庙";
+        break;
+    }// Implement the logic to change the place of the character.
 }
 
 void Linchong::printWeapons() const
--- map11.h
+++ map11.h
@@ -2,6 +2,6 @@
 class Map
 {
 public:
-    void showmap(int placeflag);//显示地图
+    void showmap(int placeflag);//显示地图，传入character中地址蚕食
     void move(int);//移动
 };
