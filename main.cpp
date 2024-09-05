77b2bab5a004e6481e2a47bac7ed91cfd89d6bc7
Shawn-ouc <1403951639@qq.com>
2024-09-05 09:24:06 +0800

    1

--- main.cpp
+++ main.cpp
@@ -9,21 +9,15 @@ struct Time
 {
     int plottime=1;
 };
+
+void shopset();
 int main()
 {
     UI ui;
     Map map;
     Plot plot;
     Time t;
-    ui.ready();
-    ui.showstart();//开始界面
-    plot.PrintPrompt();//按ctrl加速
-    plot.PrintPrologue();//前情提要
-    plot.PrintScene(t.plottime);//第一幕
-    ++t.plottime;
-    plot.PrintScene(t.plottime);//第二幕
-    ++t.plottime;
-    plot.PrintScene(t.plottime);//第三幕
+    shopset();
 
 }
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
