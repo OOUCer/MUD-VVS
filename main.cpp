7ff10c6eea6fbd42a1be2f28de242b01e03f21d9
smalltur <15159896145@163.com>
2024-09-06 15:29:22 +0800

    修改技能名字。。。。。。。。。。。。

--- main.cpp
+++ main.cpp
@@ -35,8 +35,8 @@ int change2();
     Shop1 shop1;
     Shop2 shop2;
     vector<Character*> enemies;
-    Character* character1 = new Character("宋江", 40, 1000, 1000, 40, 40, 0.8, 0.1, 0, false);
-    Character* character2 = new Character("鲁智深", 40, 2000, 1000, 40, 40, 0.8, 0.1, 0, false);
+    Character* character1 = new Character("土匪1", 40, 100, 100, 10, 10, 0.8, 0.1, 100, false);
+    Character* character2 = new Character("土匪2", 40, 100, 100, 10, 10, 0.8, 0.1, 100, false);
 
     Weapon weapon1("剑", 20, 30, 10, 0.1, "斩击");
     Weapon weapon2("刀", 20, 30, 10, 0.1, "斩击");
@@ -48,24 +48,24 @@ int change2();
     Armor armor2("防具2", 20, 30, 100, "贯通");
     Armor armor3("防具3", 20, 30, 100, "打击");
 
-    offensiveSkill skill_1("天上天下天地无双斩", "疯狂连击6次，自己防御下降", false, true, true, 0.7, "斩击", 6, selfDefenseBoost, 0.8, 3);
-    offensiveSkill skill_2("百鸟朝凤", "单体攻击5次，自己攻击下降", false, true, true, 0.8, "贯通", 5, selfAttackBoost, 0.8, 3);
-    offensiveSkill skill_3("巨人之战", "巨额全体攻击，损耗自己生命", true, true, true, 3.4, "打击", 1, selfDamage, 0.2, 0);
-    offensiveSkill skill_4("光刃暴雨", "全体攻击3次", true, true, true, 0.7, "斩击", 3, none, 0, 0);
-    offensiveSkill skill_5("粉碎打击", "粉碎单体防御", false, true, false, 2.2, "打击", 1, giveDefenseWeaken, 0.8, 3);
+    offensiveSkill skill_1("刀光剑影", "疯狂连击6次，自己防御下降", false, true, true, 0.7, "斩击", 6, selfDefenseBoost, 0.8, 3);
+    offensiveSkill skill_2("枪出如龙", "单体攻击5次，自己攻击下降", false, true, true, 0.8, "贯通", 5, selfAttackBoost, 0.8, 3);
+    offensiveSkill skill_3("登锋陷阵", "巨额全体攻击，损耗自己生命", true, true, true, 3.4, "打击", 1, selfDamage, 0.2, 0);
+    //offensiveSkill skill_4("光刃暴雨", "全体攻击3次", true, true, true, 0.7, "斩击", 3, none, 0, 0);
+    //offensiveSkill skill_5("粉碎打击", "粉碎单体防御", false, true, false, 2.2, "打击", 1, giveDefenseWeaken, 0.8, 3);
     offensiveSkill skill_6("横扫千军", "全体攻击2次", true, true, true, 1.2, "贯通", 2, none, 0, 0);
 
-    supportSkill skill_7("睡觉", "恢复体力，自身陷入昏迷状态", false, false, true, selfComatose, 1, 3);
-    supportSkill skill_8("蓄力", "下回合伤害猛增", false, false, true, selfCharge, 1, 1);
-    supportSkill skill_9("看破", "识破攻击，不被击中弱点", false, false, true, selfAnticipate, 1, 1);
-    supportSkill skill_10("剑舞", "自身攻击力上升", false, false, true, selfAttackBoost, 1.3, 3);
-    supportSkill skill_11("铁壁", "自身防御上升", false, false, true, selfDefenseBoost, 1.3, 3);
-    supportSkill skill_12("鹰眼", "自身命中率上升", false, false, true, selfHitRateBoost, 1.3, 3);
-    supportSkill skill_13("跳舞", "自身回避率上升", false, false, true, selfEvationBoost, 1.3, 3);
-    supportSkill skill_14("威吓", "敌人攻击力下降", true, false, false, giveAttackWeaken, 0.8, 3);
-    supportSkill skill_15("泼沙", "敌人命中率下降", true, false, false, giveHitRateWeaken, 0.8, 3);
-    supportSkill skill_16("摇篮曲", "可能时敌人昏迷", true, false, false, giveComatose, 0.75, 3);
-    supportSkill skill_17("清除浓雾", "使敌人回避率下降", true, false, false, giveEvationWeaken, 0.7, 3);
+    supportSkill skill_7("休养生息", "恢复体力，自身陷入昏迷状态", false, false, true, selfComatose, 1, 3);
+    supportSkill skill_8("以逸待劳", "下回合伤害猛增", false, false, true, selfCharge, 1, 1);
+    supportSkill skill_9("身经百战", "识破攻击，不被击中弱点", false, false, true, selfAnticipate, 1, 1);
+    supportSkill skill_10("蓄势待发", "自身攻击力上升", false, false, true, selfAttackBoost, 1.3, 3);
+    supportSkill skill_11("铜头铁臂", "自身防御上升", false, false, true, selfDefenseBoost, 1.3, 3);
+    supportSkill skill_12("炯炯有神", "自身命中率上升", false, false, true, selfHitRateBoost, 1.3, 3);
+    supportSkill skill_13("身轻如燕", "自身回避率上升", false, false, true, selfEvationBoost, 1.3, 3);
+    //supportSkill skill_14("威吓", "敌人攻击力下降", true, false, false, giveAttackWeaken, 0.8, 3);
+    //supportSkill skill_15("泼沙", "敌人命中率下降", true, false, false, giveHitRateWeaken, 0.8, 3);
+    supportSkill skill_16("纵兵劫掠", "可能时敌人昏迷", true, false, false, giveComatose, 0.75, 3);
+    supportSkill skill_17("破阵曲", "使敌人回避率下降", true, false, false, giveEvationWeaken, 0.7, 3);
 
 
 
@@ -146,8 +146,8 @@ void pre_battle(vector<Character*>&enemies, Linchong*lc, Character*character1, C
     lc->addOffensiveSkill(&skill_1);
     lc->addOffensiveSkill(&skill_2);
     lc->addOffensiveSkill(&skill_3);
-    lc->addOffensiveSkill(&skill_4);
-    lc->addOffensiveSkill(&skill_5);
+    //lc->addOffensiveSkill(&skill_4);
+    //lc->addOffensiveSkill(&skill_5);
     lc->addOffensiveSkill(&skill_6);
 
     lc->addSupportSkill(&skill_7);
@@ -157,8 +157,8 @@ void pre_battle(vector<Character*>&enemies, Linchong*lc, Character*character1, C
     lc->addSupportSkill(&skill_11);
     lc->addSupportSkill(&skill_12);
     lc->addSupportSkill(&skill_13);
-    lc->addSupportSkill(&skill_14);
-    lc->addSupportSkill(&skill_15);
+    //lc->addSupportSkill(&skill_14);
+    //lc->addSupportSkill(&skill_15);
     lc->addSupportSkill(&skill_16);
     lc->addSupportSkill(&skill_17);
 
