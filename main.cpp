1acd42132f3d37a0cd11d59866c984ea5255a44d
HHHwbjk <cl74384@hotmail.com>
2024-09-07 21:52:36 +0800

    添加默认武器装备

--- main.cpp
+++ main.cpp
@@ -25,7 +25,6 @@ void pre_battle2(vector<Character*>&enemies, Linchong* lc, Character* character1
 void change1();//幕与幕之间衔接
 int change2();
 
-
     UI ui;
     Map map1;
     Linchong linchong("林冲", 80, 800, 800, 10, 80, 0.8, 0.1, 100, true, 100);
@@ -47,10 +46,12 @@ int change2();
     Weapon weapon3("棍", 20, 30, 10, 0.1, "打击");
     Weapon weapon4("矛", 20, 30, 10, 0.1, "贯通");
     Weapon weapon5("花枪", 20, 30, 10, 0.1, "贯通");
+    Weapon weapon_default("林冲的花枪", 20, 30, 10, 0.1, "贯通");
 
     Armor armor1("防具1", 20, 30, 100, "斩击");
     Armor armor2("防具2", 20, 30, 100, "贯通");
     Armor armor3("防具3", 20, 30, 100, "打击");
+    Armor armor_default("林冲的布甲", 20, 30, 100, "斩击");
 
     offensiveSkill skill_1("刀光剑影", "疯狂连击6次，自己防御下降", false, true, true, 0.7, "斩击", 6, selfDefenseBoost, 0.8, 3);
     offensiveSkill skill_2("枪出如龙", "单体攻击5次，自己攻击下降", false, true, true, 0.8, "贯通", 5, selfAttackBoost, 0.8, 3);
@@ -73,20 +74,14 @@ int change2();
 
 
 
-
-
-
-
-
-
-
 int main(){
     //测试战斗系统________________
     //pre_battle(enemies,  lc, character1,character2);
     int flag;
     //PlaySound(TEXT("309764634.wav"), NULL, SND_FILENAME | SND_ASYNC);
     PlaySound(TEXT("309764634.wav"), NULL, SND_FILENAME | SND_LOOP | SND_ASYNC);
-
+    lc->addWeapon(&weapon_default);//林冲添加默认武器
+    lc->addArmor(&armor_default);//林冲添加默认装备
     // 这里可以添加你的主程序代码  
     // 由于使用了SND_ASYNC，所以音乐会在后台播放  
     // 如果你想让程序等待直到用户关闭，可以使用以下代码  
